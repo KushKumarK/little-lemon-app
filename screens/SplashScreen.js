9cbb214d1abec1e9bc24b9989489463ce23bbf01
KushKumarK <kushkumar.kushwaha2021@vitstudent.ac.in>
2023-09-22 12:11:52 +0530

    25

---
+++ screens/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+export default function SplashScreen(){
+    return (
+        <View style = {{alignContent:'center'}}>
+            <Text>Splash Screen</Text>
+        </View>
+    )
+}
